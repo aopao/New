0c142f2df8fb11ebd8982683829bb49b05d31ba3
aopao <admin@wujinyu.com>
2018-06-14 14:10:33 +0800

    auth init

---
+++ database/migrations/2018_06_14_140311_create_admin_menu_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('icon')->nullable();
+            $table->string('name');
+            $table->string('url');
+            $table->string('contorller')->nullable();
+            $table->string('action')->nullable();
+            $table->string('desc')->nullable();
+            $table->integer('parent_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_menu');
+    }
+}
