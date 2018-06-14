9eb61f6f910889c69fd9575252f7608d1efc566c
aopao <admin@wujinyu.com>
2018-06-14 13:53:58 +0800

    auth before

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -7,7 +7,7 @@
                     <div class="layui-card-header">@lang('user.user_edit')</div>
                     <div class="layui-card-body" pad15="2">
                         <form action="{{ route('admin.user.update',['id'=>$user_info['id']]) }}" method="post" class="layui-form layui-form-pane">
-                            <div class="layui-form-item" id="pic"  >
+                            <div class="layui-form-item" id="pic">
                                 <label class="layui-form-label" for="Thumb">@lang('user.avatar')</label>
                                 <div class="layui-input-inline">
                                     <input lay-verify="type" type="text" id="PicUrl" name="avatar" value="{{$user_info['avatar']}}" autocomplete="off" class="layui-input"/>
@@ -26,31 +26,31 @@
                             <div class="layui-form-item" id="title">
                                 <label class="layui-form-label">@lang('user.account')</label>
                                 <div class="layui-input-block">
-                                    <input   autocomplete="off" class="layui-input"  name="account" value="{{$user_info['account']}}" readonly="readonly" placeholder="@lang('user.account')" type="text"/>
+                                    <input autocomplete="off" class="layui-input" name="account" value="{{$user_info['account']}}" readonly="readonly" placeholder="@lang('user.account')" type="text"/>
                                 </div>
                             </div>
                             <div class="layui-form-item">
                                 <label class="layui-form-label">@lang('user.nickname')</label>
                                 <div class="layui-input-block">
-                                    <input  autocomplete="off" class="layui-input" name="nickname" value="{{$user_info['nickname']}}" placeholder="@lang('user.nickname')" type="text"/>
+                                    <input autocomplete="off" class="layui-input" name="nickname" value="{{$user_info['nickname']}}" placeholder="@lang('user.nickname')" type="text"/>
                                 </div>
                             </div>
                             <div class="layui-form-item">
                                 <label class="layui-form-label">@lang('user.mobile')</label>
                                 <div class="layui-input-block">
-                                    <input  autocomplete="off" class="layui-input" name="mobile" value="{{$user_info['mobile']}}" readonly="readonly" placeholder="@lang('user.mobile')" type="text"/>
+                                    <input autocomplete="off" class="layui-input" name="mobile" value="{{$user_info['mobile']}}" readonly="readonly" placeholder="@lang('user.mobile')" type="text"/>
                                 </div>
                             </div>
                             <div class="layui-form-item">
                                 <label class="layui-form-label">@lang('user.email')</label>
                                 <div class="layui-input-block">
-                                    <input  autocomplete="off" class="layui-input" name="email" value="{{$user_info['email']}}" placeholder="@lang('user.email')" type="text"/>
+                                    <input autocomplete="off" class="layui-input" name="email" value="{{$user_info['email']}}" placeholder="@lang('user.email')" type="text"/>
                                 </div>
                             </div>
                             <div class="layui-form-item">
                                 <label class="layui-form-label">@lang('user.qq')</label>
                                 <div class="layui-input-block">
-                                    <input  autocomplete="off" class="layui-input" name="qq" value="{{$user_info['qq']}}" placeholder="@lang('user.qq')" type="text"/>
+                                    <input autocomplete="off" class="layui-input" name="qq" value="{{$user_info['qq']}}" placeholder="@lang('user.qq')" type="text"/>
                                 </div>
                             </div>
                             <div class="layui-form-item" pane="">
@@ -60,7 +60,7 @@
                                         <input type="radio" name="status" lay-filter="status" value="1" title="@lang('user.status_on')" checked="">
                                         <input type="radio" name="status" value="0" lay-filter="status" title="@lang('user.status_off')">
                                     @else
-                                        <input type="radio" name="status" lay-filter="status" value="1" title="@lang('user.status_on')" >
+                                        <input type="radio" name="status" lay-filter="status" value="1" title="@lang('user.status_on')">
                                         <input type="radio" name="status" value="0" lay-filter="status" title="@lang('user.status_off')" checked="">
                                     @endif
                                 </div>
@@ -85,12 +85,12 @@
             base: '/theme/' //静态资源所在路径
         }).extend({
             index: 'lib/index' //主入口模块
-        }).use(['index', 'form','upload','laydate'], function () {
+        }).use(['index', 'form', 'upload', 'laydate'], function () {
             var form = layui.form
-                    , $ = layui.jquery
-                    , upload = layui.upload
-                    , index = parent.layer.getFrameIndex(window.name) //获取窗口索引
-                    , laydate = layui.laydate;
+                , $ = layui.jquery
+                , upload = layui.upload
+                , index = parent.layer.getFrameIndex(window.name) //获取窗口索引
+                , laydate = layui.laydate;
 
             //执行一个laydate实例
             laydate.render({
@@ -126,27 +126,27 @@
                     layer.msg('上传失败!', {offset: '100px', icon: 4, time: 2000});
                 }
             });
-
-
+            
+            
             @if ($errors->has('account'))
-                $('input[name="account"]').focus();
-            $('input[name="account"]:focus').css({ "cssText": "border-color:red !important" });
-            $('input[name="account"]').attr('placeholder','{{ $errors->first('account') }}');
+            $('input[name="account"]').focus();
+            $('input[name="account"]:focus').css({"cssText": "border-color:red !important"});
+            $('input[name="account"]').attr('placeholder', '{{ $errors->first('account') }}');
             @elseif($errors->has('mobile'))
-                $('input[name="mobile"]').focus();
-            $('input[name="mobile"]:focus').css({ "cssText": "border-color:red !important" });
-            $('input[name="mobile"]').attr('placeholder','{{ $errors->first('mobile') }}');
+            $('input[name="mobile"]').focus();
+            $('input[name="mobile"]:focus').css({"cssText": "border-color:red !important"});
+            $('input[name="mobile"]').attr('placeholder', '{{ $errors->first('mobile') }}');
             @elseif($errors->has('email'))
-                $('input[name="email"]').focus();
-            $('input[name="email"]:focus').css({ "cssText": "border-color:red !important" });
-            $('input[name="email"]').attr('placeholder','{{ $errors->first('email') }}');
+            $('input[name="email"]').focus();
+            $('input[name="email"]:focus').css({"cssText": "border-color:red !important"});
+            $('input[name="email"]').attr('placeholder', '{{ $errors->first('email') }}');
             @elseif($errors->has('qq'))
-                $('input[name="qq"]').focus();
-            $('input[name="qq"]:focus').css({ "cssText": "border-color:red !important" });
-            $('input[name="qq"]').attr('placeholder','{{ $errors->first('qq') }}');
+            $('input[name="qq"]').focus();
+            $('input[name="qq"]:focus').css({"cssText": "border-color:red !important"});
+            $('input[name="qq"]').attr('placeholder', '{{ $errors->first('qq') }}');
             @endif
-
-
+            
+            
             @if(Session::has('message'))
             layer.msg('{{Session::get("message")}}', {offset: '100px', icon: 1, time: 1000});
             setTimeout(function () {
@@ -154,7 +154,7 @@
                 parent.layer.close(index);
 
             }, 500);
-
+            
             @endif
 
         });
